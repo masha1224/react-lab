82eaeb0ab288c665cf92ddb8696199685369f1ca
masha1224 <maria.sgrbv@gmail.com>
2025-01-12 10:36:08 +0100

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 // F12 sprawdz konsole JS na stronie
 
+import {useState} from "react";
 function App() {
-    let title = 'Wall-E';
+    const [title, setTitle] = useState('Wall-E');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value);
+    }
+
+    let message;
+    if (title.length < 5) {
+        message = <div>Tutuł jest za krótki. Nagrywają takie filmy?</div>;
+    } else if (title.length < 15) {
+        message = <div>Tytuł jest ekstra, w sam raz na plakat przed kinem!</div>;
+    } else {
+        message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
     }
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+            <input type="text" value={title} onChange={handleChange}/>
+            {/* Wyświetlanie wiadomości, jeśli tytuł nie jest pusty */}
+            {title.length > 0 && <div>{message}</div>}
         </div>
     );
 }
+
 export default App;
